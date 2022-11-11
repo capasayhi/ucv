9e9da47081f2afc3c0c40792dc418a643db33ea7
Carlos Parra <ca.parra1611@gmail.com>
2022-11-11 17:22:43 -0400

    secondPractice improvements

--- secondPractice.py
+++ secondPractice.py
@@ -1,5 +1,4 @@
 print("Calculemos algo!")
-
 operator = input('¿Qué operación quieres realizar? (+,-,*,/): ')
 
 if operator == '+' or operator == '-' or operator == '*':
@@ -8,25 +7,21 @@ if operator == '+' or operator == '-' or operator == '*':
     second_operand = int(input('Ingresa el segundo valor: '))
 
     if operator == '+':
-        print('El resultado es: ')
-        print(first_operand + second_operand)
-    if operator == '-':
-        print('El resultado es: ')
-        print(first_operand - second_operand)
-    if operator == '*':
-        print('El resultado es: ')
-        print(first_operand * second_operand)
+        print('El resultado es: ', first_operand + second_operand)
+    elif operator == '-':
+        print('El resultado es: ', first_operand - second_operand)
+    else:
+        print('El resultado es: ', first_operand * second_operand)
 
 if operator == '/':
 
     divider = int(input('Ingresa el numerador: '))
     dividend = int(input('ingresa el denominador: '))
 
-    if dividend == 0:
-        print('Tal parece que no estudias ingeniería... intenta de nuevo ingresando otro valor')
-    else:
-        print('El resultado es: ')
-        print(divider/dividend)
-else:
+    try:
+        print('El resultado es: ', divider/dividend)
+    except ZeroDivisionError:
+        print('Tal parece que no estudias ingeniería... intenta de nuevo ingresando otro valor al denominador')
+
+if operator != '+' and operator != '-' and operator != '*' and operator != '/':
     print('No te pases de listo, ingresa una de las operaciones sugeridas')
-# Ya me di cuenta que para ese último mensaje tenia que haber usado otra condicional de la forma: if operator != '+' and operator != '-' and operator != '*' and operator != '/'. Ya que el mensaje se ejecuta siempre que la operación sea distinta de '/'
