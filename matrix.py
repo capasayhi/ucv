8ad374bf4d52e7606cc833bdbdb36664977384f6
Carlos Parra <capasayhi@gmail.com>
2023-03-01 09:25:13 -0400

    second proyect updateds

--- matrix.py
+++ matrix.py
@@ -1,20 +1,121 @@
 import numpy as np
 # https: // numpy.org/doc/stable/reference/index.html    / reference for the library
 
-from modules.algebra import matrixMultiplication, inversionOfMatrix
+from modules.algebra import matrixMultiplication, inversionOfMatrix, crossProduct, transpose, linealSist
 
-print('Avaliable operations for matrix are\n  \tMultiplication (m)\n \tInversion of matrices (i)\n \tVector product (p)\n \tTransposition of matrices (t)\n \tSystem resolution of linear equations (s)\n')
+print('Avaliable operations for matrix are\n  \tMultiplication (m)\n \tInversion of matrices (i)\n \Cross product (c)\n \tTransposition of matrices (t)\n \tSystem resolution of linear equations (s)\n')
+
+
+A = np.array([[4, 3, 2], [-2, 2, 3], [3, -5, 2]])
+B = np.array([25, -10, -4])
+X = np.linalg.inv(A).dot(B)
+
+print(X)
 
 operation = input('What operation do you want to perform?: ')
 
 # generate 2 matrix of random values
-A = np.random.randint(1, 10, size=(3, 3))
-B = np.random.randint(1, 10, size=(3, 2))
-print(f"Matrix A:\n {A}\n")
-print(f"Matrix B:\n {B}\n")
+# A = np.random.randint(1, 10, size=(3, 3))
+# B = np.random.randint(1, 10, size=(3, 2))
+# print(f"Matrix A:\n {A}\n")
+# print(f"Matrix B:\n {B}\n")
 
-C = matrixMultiplication(A, B)
-print(f"Result Matrix (C):\n {C}\n")
+if operation == 'm':
+    print('\nTo first matrix (A)')
+    ra = int(input("Enter the number of rows:"))
+    ca = int(input("Enter the number of columns:"))
 
-I = inversionOfMatrix(A)
-print(f'Inversion of A:\n {I}\nl')
+    # User input of entries in a single line separated by space
+    print("Enter the entries in a single line (separated by space): ")
+    entriesA = list(map(int, input().split()))
+    A = np.array(entriesA).reshape(ra, ca)
+
+    print('\nTo second matrix (B)')
+    rb = int(input("Enter the number of rows:"))
+    cb = int(input("Enter the number of columns:"))
+    orderA = tuple((ra, ca))
+    orderB = tuple((rb, cb))
+    print("Enter the entries in a single line (separated by space): ")
+    entriesB = list(map(int, input().split()))
+    B = np.array(orderB).reshape(rb, cb)
+
+    C = matrixMultiplication(entriesA, orderA, entriesB, orderB)
+    print(f"Result Matrix (C):\n {C}\n")
+
+
+elif operation == 'i':
+    print('\nTo matrix (M)')
+    r = int(input("Enter the number of rows:"))
+    c = int(input("Enter the number of columns:"))
+
+    # User input of entries in a single line separated by space
+    print("Enter the entries in a single line (separated by space): ")
+    entries = list(map(int, input().split()))
+    M = np.array(entries).reshape(r, c)
+    print(f"Matrix (M):\n {M}\n")
+
+    I = inversionOfMatrix(M)
+    print(f'Inversion of Matrix ( M^-1 ): \n {I}\nl')
+
+elif operation == 'c':
+
+    # User input of entries in a single line separated by space
+    print("Enter the entries of the first vector in a single line (separated by space): ")
+    entriesX = list(map(int, input().split()))
+    if len(entriesX) < 2 or len(entriesX) > 3:
+        raise NameError(
+            'Incompatible dimensions for cross product, dimension must be 2 or 3')
+    else:
+        X = np.array(entriesX)
+
+        print("Now the entries of the other vector: ")
+        entriesY = list(map(int, input().split()))
+        if len(entriesY) < 2 or len(entriesY) > 3:
+            raise NameError(
+                'Incompatible dimensions for cross product, dimension must be 2 or 3')
+        else:
+            Y = np.array(entriesY)
+            cross = crossProduct(X, Y)
+            print(f'Cross product of: {X} , {Y} is\n', cross)
+
+elif operation == 't':
+
+    print('\nTo matrix (A)')
+    ra = int(input("Enter the number of rows:"))
+    ca = int(input("Enter the number of columns:"))
+
+    # User input of entries in a single line separated by space
+    print("Enter the entries in a single line (separated by space): ")
+    entriesA = list(map(int, input().split()))
+
+    if len(entriesA) < ra*ca or len(entriesA) > ra*ca:
+        raise NameError(
+            f'Invalid input, must enter {ra*ca} values')
+    else:
+        A = np.array(entriesA).reshape(ra, ca)
+        transpose = transpose(A)
+        print(f'Transpose of: \n\n{A} \n\nis\n\n', transpose)
+
+elif operation == 's':
+    order = int(input('Enter the order of the coeficients matrix: '))
+    print(order**2)
+    print("Enter the entries of coeficients matrix in a single line (separated by space): ")
+    entriesC = list(map(int, input().split()))
+    if len(entriesC) < order ** 2 or len(entriesC) > order ** 2:
+        raise NameError(
+            f'Invalid input, must enter {order**2} values')
+    else:
+        coeficientsMatrix = np.array(entriesC).reshape(order, order)
+        print(f"Coeficients matrix:\n {coeficientsMatrix}\n")
+        print("Enter the entries of results in a single line (separated by space): ")
+        entriesR = list(map(int, input().split()))
+        if len(entriesR) < order or len(entriesR) > order:
+            raise NameError(
+                f'Invalid input, it must be at least {order} values')
+        else:
+            results = np.array(entriesR).reshape(order, 1)
+        print(results)
+        R = linealSist(coeficientsMatrix, results)
+        print(f'Result of the system of linear equations  : \n\n{R}')
+else:
+    raise NameError('Invalid input, check the character you are typing')
--- modules/algebra.py
+++ modules/algebra.py
@@ -3,19 +3,42 @@ import numpy as np
 
 
 def matrixMultiplication(A, B):
-    global C
     if A.shape[1] == B.shape[0]:  # Validates if A has as many columns as B has rows
         C = np.array([[sum(r*c for r, c in zip(rowsMatrixA, columnsMatrixB)) for columnsMatrixB in zip(*B)]
                       for rowsMatrixA in A])
         return C
     else:
-        return "Ups! cannot multiply A and B. Check the order of the matrices."
+        raise NameError(
+            "Ups! cannot multiply A and B. Check the order of the matrices.")
 
 
-def inversionOfMatrix(M):
+def inversionOfMatrix(m):
+    I = []
+    M = np.array(m)
     if M.shape[0] == M.shape[1]:
-        try:
-            I = np.linalg.inv(M)
-            return I
-        except:
-            raise NameError("Check the order of the matrices.")
+        I = np.linalg.inv(M)
+        return I
+    else:
+        raise NameError("Check the order of the matrices.")
+
+
+def crossProduct(x, y):
+    X = np.array(x)
+    Y = np.array(y)
+    product = np.cross(X, Y)
+    return product
+
+
+def transpose(m):
+    M = np.array(m)
+    transpose = M.transpose()
+    return transpose
+
+
+def linealSist(A, B):  # (Coeficients matrix, Results list)
+
+    if A.shape[1] == B.shape[0]:
+        X = np.linalg.inv(A).dot(B)
+        return X
+    else:
+        raise NameError("Check the order of the matrices.")
